27b846fd323a9e55c9fc70ecd4f2f897ea6152de
ucfilho <42356381+ucfilho@users.noreply.github.com>
2020-10-13 01:48:00 -0300

    Update edo_rk4.py

--- edo_rk4.py
+++ edo_rk4.py
@@ -15,9 +15,9 @@ def rk4( f, x0, t ):
     for i in range( n - 1 ): 
         
         k1 = h * f( x[i,:], t[i] )
-        k2 = h * f( x[i,:] + 0.5 * k1[j], t[i] + 0.5 * h )
-        k3 = h * f( x[i,:] + 0.5 * k2[j], t[i] + 0.5 * h )
-        k4 = h * f( x[i,:] + k3[j], t[i+1] )
+        k2 = h * f( x[i,:] + 0.5 * k1, t[i] + 0.5 * h )
+        k3 = h * f( x[i,:] + 0.5 * k2, t[i] + 0.5 * h )
+        k4 = h * f( x[i,:] + k3, t[i+1] )
         
         for j in range(nx):
             x[i+1,j] = x[i,j] + ( k1[j] + 2.0 * ( k2[j]  + k3[j]  ) + k4[j]  ) / 6.0
