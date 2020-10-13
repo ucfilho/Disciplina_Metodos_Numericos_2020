2ecd6f25034fe035678c424ebcb99893c2782595
ucfilho <42356381+ucfilho@users.noreply.github.com>
2020-10-13 02:05:52 -0300

    Update edo_rk4.py

--- edo_rk4.py
+++ edo_rk4.py
@@ -14,13 +14,13 @@ def rk4( f, x0, t ):
     
     for i in range( n - 1 ):
         xnew =[x[i,j] for j in range(nx)]
-        k1 = h * f( xnew, t[i] )
+        k1 = np.multiply(h ,f( xnew, t[i] ))
         xnew =[x[i,j]+0.5*k1[j] for j in range(nx)]
-        k2 = h * f( xnew, t[i] + 0.5 * h )
+        k2 = np.multiply(h ,f( xnew, t[i] + 0.5 * h ))
         xnew =[x[i,j]+0.5*k2[j] for j in range(nx)]
-        k3 = h * f( xnew, t[i] + 0.5 * h )
+        k3 = np.multiply(h ,f( xnew, t[i] + 0.5 * h ))
         xnew =[x[i,j]+ k3[j] for j in range(nx)]
-        k4 = h * f(xnew, t[i+1] )
+        k4 = np.multiply(h ,f(xnew, t[i+1] ))
       
         for j in range(nx):
             x[i+1,j] = x[i,j] + ( k1[j] + 2.0 * ( k2[j]  + k3[j]  ) + k4[j]  ) / 6.0
