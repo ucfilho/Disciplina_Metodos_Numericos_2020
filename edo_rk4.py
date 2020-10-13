edac41e3afa42cde5be80b80211c446eb22d02dc
ucfilho <42356381+ucfilho@users.noreply.github.com>
2020-10-13 02:00:20 -0300

    Update edo_rk4.py

--- edo_rk4.py
+++ edo_rk4.py
@@ -9,20 +9,19 @@ def rk4( f, x0, t ):
     k2 = np.zeros(nx, dtype = float) 
     k3 = np.zeros(nx, dtype = float) 
     k4 = np.zeros(nx, dtype = float) 
-    
+    xnew = np.zeros(nx, dtype = float) 
     h = t[1] - t[0]
     
-    for i in range( n - 1 ): 
-        
-        #k1 = h * f( x[i,:], t[i] )
-        #k2 = h * f( x[i,:] + 0.5 * k1, t[i] + 0.5 * h )
-        #k3 = h * f( x[i,:] + 0.5 * k2, t[i] + 0.5 * h )
-        #k4 = h * f( x[i,:] + k3, t[i+1] )
-        k1 = f( x[i,:], t[i] )
-        k2 = f( x[i,:] +k1, t[i] + 0.5 * h )
-        k3 = f( x[i,:] + k2, t[i] + 0.5 * h )
-        k4 = f( x[i,:] + k3, t[i+1] )        
-        
+    for i in range( n - 1 ):
+        xnew =[x[i,j] for j in range(nx)]
+        k1 = h * f( xnew, t[i] )
+        xnew =[x[i,j]+0.5*k1[j] for j in range(nx)]
+        k2 = h * f( xnew, t[i] + 0.5 * h )
+        xnew =[x[i,j]+0.5*k2[j] for j in range(nx)]
+        k3 = h * f( xnew, t[i] + 0.5 * h )
+        xnew =[x[i,j]+ k3[j] for j in range(nx)]
+        k4 = h * f(xnew, t[i+1] )
+      
         for j in range(nx):
             x[i+1,j] = x[i,j] + ( k1[j] + 2.0 * ( k2[j]  + k3[j]  ) + k4[j]  ) / 6.0
 
