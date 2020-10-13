85d053d7313657989d7b4ed446d857e6d4e89033
ucfilho <42356381+ucfilho@users.noreply.github.com>
2020-10-13 01:43:24 -0300

    Update edo_rk4.py

--- edo_rk4.py
+++ edo_rk4.py
@@ -9,9 +9,6 @@ def rk4( f, x0, t ):
     k2 = np.zeros(nx, dtype = float) 
     k3 = np.zeros(nx, dtype = float) 
     k4 = np.zeros(nx, dtype = float) 
-    print('======');print(k1);print(glx)
-    
-  
     
     h = t[1] - t[0]
     
